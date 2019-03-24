bf2f2825f410af3ef4ec013ebd9b7bc21df7bc53
Kuldeep Singh <kuldeepsinghk.com>
2019-03-24 12:22:23 +0530

    get the bank to transaction mapping working

--- src/data_str_clojure/core.clj
+++ src/data_str_clojure/core.clj
@@ -1,6 +1,7 @@
 (ns data-str-clojure.core
   (:gen-class)
-  (:require [dk.ative.docjure.spreadsheet :as spreadsheet]))
+  (:require [dk.ative.docjure.spreadsheet :as spreadsheet])
+  (:require [clojure.reflect :as r]))
 
 
 
@@ -10,6 +11,18 @@
     (spreadsheet/select-sheet "NEFT")
     (spreadsheet/select-columns {:C :bankName :D :numOfTransaction})))
 
+(defn valid-row? [col]
+  (and (not (nil? (:bankName col)))
+       (not (= "BANK" (str (:bankName col))))
+       )
+  )
+
+(defn all-valid-rows []
+  (->>
+    (read-rows)
+    (rest)
+    (filter valid-row?)))
+
 (defn -main
   [& args]
-  (println type (read-rows)))
+  (println (doall (sort-by last (drop-last (all-valid-rows))))))
