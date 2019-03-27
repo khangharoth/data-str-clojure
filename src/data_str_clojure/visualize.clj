c3ad0d21a17a719a47cd7fed50489283170b48b2
Kuldeep Singh <kuldeepsinghk.com>
2019-03-27 20:48:59 +0530

    git ignore

--- src/data_str_clojure/visualize.clj
+++ src/data_str_clojure/visualize.clj
@@ -5,20 +5,29 @@
 
 (oz/start-plot-server!)
 
+(defn coeff [name]
+  (println name)
+  (cond
+    (= "first" name) 1
+    (= "second" name) 2
+    (= "third" name) 3
+    :else 5
+    )
+  )
+
 (defn play-data [& names]
-  (for [n names
-        i (range 20)]
-    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
+  (for [n names i (range 20)]
+    {:time i :item n :quantity (* (coeff n) i)}))
 
 (def line-plot
-  {:data     {:values (play-data "monkey" "slipper" "broom")}
+  {:data     {:values (play-data "first" "second" "third")}
    :encoding {:x     {:field "time"}
               :y     {:field "quantity"}
               :color {:field "item" :type "nominal"}}
    :mark     "line"})
 
 (println "about to plot")
-(Thread/sleep 5000)
+(Thread/sleep 1000)
 ;; Render the plot
 (oz/v! line-plot)
 
